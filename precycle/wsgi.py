6be84e8b9dfcb0bd4c1797167a8b78cd1a67f678
cmd08010 <colleendunion@gmail.com>
2021-04-18 17:50:54 -0400

    update settings

--- precycle/wsgi.py
+++ precycle/wsgi.py
@@ -11,7 +11,7 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django_auth_template.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'precycle.settings')
 
 application = get_wsgi_application()
 
